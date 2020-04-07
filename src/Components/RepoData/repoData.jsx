708aac5ceaf700bf3c6ca63077fc06ee0c7d06a9
Jose Gomez <jose.fgm1995@gmail.com>
2020-04-06 23:28:44 -0500

    Creacion de los componentes necesarios para la visualizacion de un repositorio

---
+++ src/Components/RepoData/repoData.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './repoData.scss';
+
+const RepoData = () => {
+  return <div>RepoData</div>;
+};
+
+export default RepoData;
---
+++ src/Components/RepoDescription/repoDescription.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './repoDescription.scss';
+
+const RepoDescription = () => {
+  return <div>RepoDescription</div>;
+};
+
+export default RepoDescription;
