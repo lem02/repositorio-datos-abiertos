42c19eeaab0557eb2d7da584a92241149608b803
Jose Gomez <jose.fgm1995@gmail.com>
2020-04-06 23:30:28 -0500

    Estructura basica de la vista principal de la pagina

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Repository from '../Repository/repository';
+import './home.scss';
 
 const Home = () => {
-  return <section>Home Page</section>;
+  return (
+    <section className="home">
+      <section className="home__search-result"></section>
+      <section className="home__repository-selected">
+        <Repository />
+      </section>
+    </section>
+  );
 };
 
 export default Home;
