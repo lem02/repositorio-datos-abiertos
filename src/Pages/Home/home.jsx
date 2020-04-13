81bed6bedc651b1734bf5b804fb9ebff3f9ee342
Jose Gomez <jose.fgm1995@gmail.com>
2020-04-12 22:24:29 -0500

    Consulta a la API de los repositorios actuales

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import Repository from '../Repository/repository';
+import React, { useState, useEffect } from 'react';
+import SearchResult from '../../Components/SearchResult/searchResult';
+import Repository from '../../Components/Repository/repository';
+import Request from '../../Requests/apiRequests';
 import './home.scss';
 
 const Home = () => {
+  const [data, setData] = useState();
+  const [repoSelected, setRepoSelected] = useState();
+
+  useEffect(() => {
+    Request.get('/all', (res) => {
+      setData(res);
+      setRepoSelected(res[0]);
+    });
+  }, []);
+
   return (
     <section className="home">
-      <section className="home__search-result"></section>
+      <section className="home__search-result">
+        <SearchResult
+          data={data}
+          repoSelected={repoSelected}
+          setRepoSelected={setRepoSelected}
+        />
+      </section>
       <section className="home__repository-selected">
-        <Repository />
+        <Repository repoSelected={repoSelected} />
       </section>
     </section>
   );
