08863ccd36016051d496a4d6508ff613a692ceda
Jose Gomez <jose.fgm1995@gmail.com>
2020-04-22 02:25:37 -0500

    Implementació de la logica de la barra de busqueda usando el contexto

--- src/Components/SearchBar/searchBar.jsx
+++ src/Components/SearchBar/searchBar.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { SiteContext } from '../../Context/siteContext';
+import Request from '../../Requests/apiRequests';
 import './searchBar.scss';
 
 const SearchBar = () => {
+  const { setDataResult } = useContext(SiteContext);
+  const [search, setSearch] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Request.get(`/findbykey/${search}`, (res) => {
+      if (res.length < 1) {
+        window.alert(`No se encontraron resultados para: ${search}`);
+      } else {
+        setDataResult(res);
+      }
+    });
+  };
+
   return (
-    <div className="search-bar">
-      <input type="text" />
-      <i className="fa fa-search" />
-    </div>
+    <form className="search-bar" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <button type="submit" className="search-bar__button">
+        <i className="fa fa-search" />
+      </button>
+    </form>
   );
 };
 export default SearchBar;
