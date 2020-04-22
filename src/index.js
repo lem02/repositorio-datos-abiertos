e7ef2ed3cd21510e40506e4ca1a7ca5fe1975f75
Jose Gomez <jose.fgm1995@gmail.com>
2020-04-22 02:21:24 -0500

    Creación del contexto global del sitio

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import SiteContextContainer from './Context/siteContext';
 import App from './App';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <SiteContextContainer>
+      <App />
+    </SiteContextContainer>
   </BrowserRouter>,
   document.getElementById('root')
 );
