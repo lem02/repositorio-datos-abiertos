5d0039457c656980530219f0f13112c610a11197
Jose Gomez <jose.fgm1995@gmail.com>
2020-04-22 02:23:04 -0500

    El logo lleva al home

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Cart from '../Cart/cart';
 import SearchBar from '../SearchBar/searchBar';
 import './header.scss';
@@ -6,12 +7,12 @@ import './header.scss';
 const Header = () => {
   return (
     <header className="header">
-      <figure className="header__logo">
+      <Link to="/" className="header__logo">
         <img
           src="http://udea.edu.co/wps/wcm/connect/udea/3ef4bbd1-4ae7-4c87-b843-685c6f017501/logo-udea.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_L8L8H8C0LODDC0A6SSS2AD2GO4-3ef4bbd1-4ae7-4c87-b843-685c6f017501-lr.XbEn"
           alt="Logo Universidad de Antioquia"
         />
-      </figure>
+      </Link>
       <SearchBar />
       <Cart />
     </header>
