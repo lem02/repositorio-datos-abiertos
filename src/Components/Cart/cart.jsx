1f4e85d6b0677f2f38b6d538ab8c9151f6af53cf
Jose Gomez <jose.fgm1995@gmail.com>
2020-04-22 02:22:24 -0500

    Implementación del carrito de compras usando el contexto con datos de sesion

--- src/Components/Cart/cart.jsx
+++ src/Components/Cart/cart.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { SiteContext } from '../../Context/siteContext';
 import './cart.scss';
 
 const Cart = () => {
+  const { order, deleteItem } = useContext(SiteContext);
+
   return (
     <div className="cart">
-      <div className="cart__icon">
+      <div
+        className={`cart__icon ${order.length > 0 ? 'cart__icon--move' : ''}`}
+      >
         <i className="fa fa-shopping-cart" />
-        <span>2</span>
+        <span>{order.length}</span>
       </div>
-      <div className="cart__items"></div>
+      <div className="cart__container">
+        <div className="cart__items">
+          {order.map((item, index) => (
+            <article key={index} className="cart__item">
+              <p>{item.titulo}</p>
+              <button onClick={() => deleteItem(item)}>
+                <i className="fa fa-trash" />
+              </button>
+            </article>
+          ))}
+        </div>
+        <div className="cart__total">
+          <Link to="/form">Solicitar </Link>
+        </div>
+      </div>
     </div>
   );
 };
